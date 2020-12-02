23fc70135d7394eaee7edf6a845d0465a3a89750
lucas merat <lucas.merat@gmail.com>
2020-12-02 12:30:48 -0500

    add account and public navs. add profile route barebones

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,12 +1,17 @@
 class AccountsController < ApplicationController
   before_action :authenticate_account!
+  before_action :set_account, only: [:profile]
 
   def index
     # dashboard feed of posts
     @posts = Post.active
   end
 
-  def show
+  def profile
     # user profile
+  end
+
+  def set_account
+    @account = Account.find_by(username: params[:username])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
+  before_create :set_active
 
   belongs_to :account
 
   scope :active, -> { where active: true }
+
+  def set_active
+    self.active = true
+  end
 end
